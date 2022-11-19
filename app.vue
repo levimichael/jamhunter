b1390c8b330b28992c95119e93e3f1f57051164c
Levi Whalen <levi@leviwhalen.com>
2022-11-19 01:56:28 -0800

    init

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid grid-cols-4">
+    <Release v-bind="track" :playing="current" v-for="track in tracks" :key="track.href"
+      @playing="updateCurrentPlayingTrack" />
+  </div>
+</template>
+<script setup lang="ts">
+const { data: tracks } = await useFetch('/api/get-tracks')
+
+const current = ref('')
+
+const updateCurrentPlayingTrack = (href: string) => {
+  current.value = href
+}
+</script>
+
---
+++ components/Release.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        {{ state }}
+        <audio :src="preview" ref="audio" hidden></audio>
+        <img :src="image" @click="togglePlay">
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    preview: {
+        type: String,
+    },
+    image: {
+        type: String
+    },
+    href: {
+        type: String
+    },
+    playing: {
+        type: String
+    }
+})
+
+const emit = defineEmits(['playing'])
+
+const audio = ref<HTMLAudioElement>()
+
+const state = ref('initial')
+
+const togglePlay = () => {
+    if (audio.value && (state.value === 'initial' || state.value === 'paused')) {
+        audio.value.play()
+        emit('playing', props.href)
+    }
+    if (audio.value && state.value === 'playing') audio.value.pause()
+}
+
+const currentlyPlaying = computed(() => {
+    return props.href === props.playing
+})
+
+watch(currentlyPlaying, (val) => {
+    if (!val) audio.value?.pause()
+})
+
+
+
+onMounted(() => {
+    if (audio.value) {
+        audio.value.addEventListener('play', () => {
+            state.value = 'playing'
+        })
+        audio.value.addEventListener('pause', () => {
+            state.value = 'paused'
+        })
+    }
+})
+
+</script>
