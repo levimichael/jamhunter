5d5d88efbed79782e596f3b1562995a24aba5ca1
Levi Whalen <levi@leviwhalen.com>
2023-03-07 08:10:43 -0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-4 gap-4 m-4">
+  <div class="grid grid-cols-5 gap-4 m-4">
     <Release v-bind="track" :playing="current" v-for="track in tracks" :key="track.href"
       @playing="updateCurrentPlayingTrack" />
   </div>
