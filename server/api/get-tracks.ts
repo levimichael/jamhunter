8fc5b6d01d5a3198e3a022846f3785d19570d773
Levi Whalen <levi@leviwhalen.com>
2024-07-10 13:55:03 -0600

    Update get-tracks.ts

--- server/api/get-tracks.ts
+++ server/api/get-tracks.ts
@@ -1,5 +1,5 @@
 import { getTracks } from '~/lib/spotify'
 export default defineEventHandler(async () => {
-  const tracks = await Promise.all([getTracks(0), getTracks(50), getTracks(100),getTracks(150), getTracks(200), getTracks(250)])
+  const tracks = await Promise.all([getTracks(0), getTracks(50), getTracks(100),getTracks(150), getTracks(200), getTracks(250), getTracks(300), getTracks(350), getTracks(400), getTracks(450)])
   return tracks.flat()
 })
